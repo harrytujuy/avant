d2ec8982994c47b7061c217428e4aed4a14dce43
harrytujuy <harry.2918@gmail.com>
2014-07-15 14:28:21 -0500

    Subiendo todos los htmls

---
+++ newsletter.php
@@ -0,0 +1,8 @@
+<?
+	// ----- Traitement newsletter ----- //
+	
+	$your_email = "example@example.com"
+	// --------------------------------- //
+	
+	header("Location: ./index.htm?page=_newsletter_message.htm");
+?>
